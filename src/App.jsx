1fc5c2e4a8e26c3375ff6d05a878e6ce00c1c50f
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-02-06 16:36:53 -0800

    hall of fame added back in

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Page from "./components/Page";
 import Interaction from "./components/Interaction";
+import HallOfFame from "./components/HallOfFame";
 import Footer from "./components/Footer";
 import { AppProvider } from "./context/AppContext";
 
@@ -12,6 +13,7 @@ const App = () => {
         <Header />
         <Page />
         <Interaction />
+        <HallOfFame />
         <Footer />
       </>
     </AppProvider>
---
+++ src/components/HallOfFame/index.jsx
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+const HallOfFame = () => {
+  return <div id="hof"></div>;
+};
+
+export default HallOfFame;
